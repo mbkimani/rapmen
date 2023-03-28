de491b605f8dcaf6c32d2627fc17fc8890cf0d0e
Brian Kimani <mbkimani@gmail.com>
2023-03-28 10:34:36 +0300

    rapper api barebones

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+const rapmen = {
+  "mwas the rapper": {
+    age: 23,
+    birthName: "Paul Mwas",
+    birthLocation: "Ruai",
+  },
+  kielekezo: {
+    age: 32,
+    birthName: "Kiele Makezo",
+    birthLocation: "Cape  Town",
+  },
+  unknown: {
+    age: 0,
+    birthName: "unknown",
+    birthLocation: "unknown",
+  },
+};
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/index.html");
+});
+
+app.get("/api/:rapman", (req, res) => {
+  const rapmister = req.params.rapman.toLocaleLowerCase();
+  if (rapmen[rapmister]) {
+    res.json(rapmen[rapmister]);
+  } else {
+    res.json(rapmen["unknown"]);
+  }
+});
+
+app.listen(process.env.PORT || PORT);
