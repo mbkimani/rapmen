7e47aef35d0b52d5a34e353ce6a38dbdf4e077de
Brian Kimani <mbkimani@gmail.com>
2023-03-28 10:47:10 +0300

    added cors to api

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const PORT = 3000;
+
+app.use(cors());
+
 const rapmen = {
   "mwas the rapper": {
     age: 23,
